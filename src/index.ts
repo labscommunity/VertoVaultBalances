72a350d39c74fa4ca3fa906b18e3f84a75b64d60
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-12-14 15:13:36 +0000

    chore: add __dirname instead of full path

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,31 @@ import * as fs from 'fs'
 
 
 // tate small numbers
-const firstNumbers = JSON.parse(fs.readFileSync('/Users/Lorimer/Desktop/Othent.io/VertoVaultBalances/src/lib/tateStuff/firstNumbers.json', 'utf-8'))
+const firstNumbers = JSON.parse(fs.readFileSync(`${__dirname}/lib/tateStuff/firstNumbers.json`, 'utf-8'))
 const firstNumbersAdded = sumValues(firstNumbers);
 console.log('firstNumbersAdded', firstNumbersAdded);
 
 
 // tate state
-const tateState = JSON.parse(fs.readFileSync('/Users/Lorimer/Desktop/Othent.io/VertoVaultBalances/src/lib/tateStuff/tateFile.json', 'utf-8'))
+const tateState = JSON.parse(fs.readFileSync(`${__dirname}/lib/tateStuff/tateFile.json`, 'utf-8'))
 const tateStateAdded = await sumVault(tateState.vault)
 console.log('tateStateAdded', tateStateAdded)
 
 
 // smartweave state
-const smartweaveState = JSON.parse(fs.readFileSync('/Users/Lorimer/Desktop/Othent.io/VertoVaultBalances/smartweaveState.json', 'utf-8'))
+const smartweaveState = JSON.parse(fs.readFileSync('./smartweaveState.json', 'utf-8'))
 const smartweaveStateAdded = await sumVault(smartweaveState.vault)
 console.log('smartweaveStateAdded', smartweaveStateAdded)
 
 
 // tom tool state
-const tomToolState = JSON.parse(fs.readFileSync('/Users/Lorimer/Desktop/Othent.io/VertoVaultBalances/src/lib/tomTool/tomToolRes.json', 'utf-8'))
+const tomToolState = JSON.parse(fs.readFileSync(`${__dirname}/lib/tomTool/tomToolRes.json`, 'utf-8'))
 const tomToolStateAdded = await sumVault(tomToolState.vault)
 console.log('tomToolStateAdded', tomToolStateAdded)
 
 
 // PPE state
-const ppeState = JSON.parse(fs.readFileSync('/Users/Lorimer/Desktop/Othent.io/VertoVaultBalances/src/lib/ppeRecommendation/ppeRecommendation.json', 'utf-8'))
+const ppeState = JSON.parse(fs.readFileSync(`${__dirname}/lib/ppeRecommendation/ppeRecommendation.json`, 'utf-8'))
 const ppeStateAdded = await sumVault(ppeState.state.vault)
 console.log('ppeStateAdded', ppeStateAdded)
 
